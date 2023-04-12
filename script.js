b88cead5b635a9a61b8b15e80e5def505cfb68e4
Gracy <g.whelihan@gmail.com>
2023-04-12 10:12:07 -0400

    add api

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+let number = 205;
+//  let url =`https://api.isevenapi.xyz/api/iseven/${number}`
+let url =`https://api.math.tools/numbers/pi`
+let pi;
+
+function getData(){
+	const options = {
+		method: 'GET',
+		headers: {
+			// 'X-RapidAPI-Key': 'your-rapidapi-key',
+			// 'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+		},
+	};
+	fetch(
+		url,
+		options
+	)
+		.then(response => response.json())
+		.then(response => {
+      pi = response.cotents.result;
+      console.log(pi);
+      document.getElementById("pi").innerHTML = pi;
+    })
+		.catch(err => console.error(err));
+  }
+
+console.log(pi);
